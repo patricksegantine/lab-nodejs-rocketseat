45128b5f08e0257cfbb803424c9fda2be24675f4
Patrick Segantine <patricksegantine@gmail.com>
2020-02-10 09:30:38 -0300

    adicionando cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();	
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o banco de dados
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
@@ -12,10 +14,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 // Importando todos os models com o plugin require-dir
 requireDir('./src/models');
 
-
 // Rotas
 app.use('/api', require('./src/routes'));
-
 
 // Inicialização do servidor
 const port = 3001;
